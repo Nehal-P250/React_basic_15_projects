152a6654803e3198c5b375dde41b69edf72652e4
vectorNP <pisarnehal@gmail.com>
2021-04-27 19:56:40 +0530

    7> 7.2 added the auto scroll /  setInterval

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -14,6 +14,27 @@ function App() {
     }
   }, [index]);
 
+  useEffect(() => {
+    // https://youtu.be/a_7Z7C_JCyo?t=10043
+    // important on why we need to call clearInterval.
+    // https://upmostly.com/tutorials/setinterval-in-react-components-using-hooks#:~:text=Using%20setInterval%20inside%20React%20components,some%20code%20at%20specific%20intervals.&text=This%20will%20schedule%20once%20the,Hook%2C%20passing%20in%20the%20interval.
+
+    // here if we dont pass the clearInterval then , every time the
+    // index is changing it will call this hook, and will continue
+    // to show animations if we click the next/ prev buttom multiple
+    // times which we dont want ,
+    // instead we clear out the previous hook and detach it so if
+    // rapid next / prev button is placed it will get cleared for
+    // the previous index value and will only be active for the next
+    // index value.
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">

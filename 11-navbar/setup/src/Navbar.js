f5551720023b76904f0acc4d5b29b8ad3564b42c
vectorNP <pisarnehal@gmail.com>
2021-05-02 18:32:07 +0530

    11> Nav bar Hardcoded way

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,58 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          {/* here the links are related to our project and are handled by the 
+        react router , so use react router to organize them */}
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+          </ul>
+        </div>
+        {/* here the links are for social media and do not depnds on our project 
+        hence fetch it form some API or keep it constant. */}
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

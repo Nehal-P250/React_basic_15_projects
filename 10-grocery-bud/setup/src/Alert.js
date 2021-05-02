a191fb94060e12df1f7edf99a5df998f310d2f3c
vectorNP <pisarnehal@gmail.com>
2021-05-02 15:16:56 +0530

    10> Groceery , IMPORTANT , local storage etc...

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-const Alert = () => {
-  return <h2>alert component</h2>
-}
+// if we dont use list in the dependency list of useEffect and only use
+// empty dependency list i.e[], then when we add the item it will get
+// rendered for the 1st time and the use effect will be in processs to
+// remove alert after 3 second , but before this 3 lets say after 1 second only ,
+// we added new item or removed a item from the list then the the alert for the
+// new action will only be shown for the remaining second i.e. the remaining 2 second,
+// which is incorrect hence we also have to add the main list in the depencency
+// array so that whenever the list updates we can erase it.
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert(false, "", "");
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
 
-export default Alert
+export default Alert;
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,123 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+import { FaGlobeAmericas } from "react-icons/fa";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  // there can be 2 cases either the list exists or it does not
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //display alert
+      showAlert(true, "danger", "please enter a value");
+    } else if (name && isEditing) {
+      // deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            // my way to doing it
+            // item.title = name;
+            //jonh way
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
+    } else {
+      //show alert that item is added
+      showAlert(true, "success", " Item added to the list");
+      // add the item to the list
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  //default value for the parmeters
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+    // it is same as
+    // setAlert({show:show,type:type,msg:msg});
+    // but as in ES6 if the type matches the actual property name
+    // we can directly mention the property name
+  };
+  const clearList = () => {
+    showAlert(true, "danger", "Empty list");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "Item Removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  //useEffect to update the loca storage , whenever anything in the list
+  //changes we update it in the local storage so it can persists between
+  //multiple time refresh or accesses.
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
